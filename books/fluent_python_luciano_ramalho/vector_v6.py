c6f83248ea9b69f0c8e2e18afa69c9134bc5687d
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-05-27 12:29:17 +0300

    Update vector_v6.py
    1. overloaded "__mul__", "__rmul__"
    2. added tests for "__mul__", "__rmul__"

--- books/fluent_python_luciano_ramalho/vector_v6.py
+++ books/fluent_python_luciano_ramalho/vector_v6.py
@@ -206,6 +206,21 @@ class Vector:
         Vector([11.0, 22.0, 33.0])
         >>> (10, 20, 30) + v1 == Vector([11.0, 22.0, 33.0])
         True
+    
+    Tests for ``__mul__``, ``__rmul__``::
+    
+        >>> v1 = Vector([1, 2, 3])
+        >>> v1 * 10
+        Vector([10.0, 20.0, 30.0])
+        >>> 11 * v1
+        Vector([11.0, 22.0, 33.0])
+        >>> 14 * v1
+        Vector([14.0, 28.0, 42.0])
+        >>> v1 * True
+        Vector([1.0, 2.0, 3.0])
+        >>> from fractions import Fraction
+        >>> v1 * Fraction(1, 3)
+        Vector([0.3333333333333333, 0.6666666666666666, 1.0])
     """
     
     typecode = 'd'
@@ -253,6 +268,15 @@ class Vector:
     
     def __radd__(self, other):
         return self + other
+    
+    def __mul__(self, scalar):
+        if isinstance(scalar, numbers.Real):
+            return Vector(n * scalar for n in self)
+        else:
+            return NotImplemented
+    
+    def __rmul__(self, scalar):
+        return self * scalar
     
     def __bool__(self):
         return bool(abs(self))
