5786912faf203b7d7e92392540f9c8a7daa2a0e2
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-05-31 10:06:54 +0300

    Create sentence_gen.py

---
+++ books/fluent_python_luciano_ramalho/sentence_gen.py
@@ -0,0 +1,42 @@
+import re
+import reprlib
+import doctest
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+    
+    """
+    class ``Sentence``
+    
+        >>> s = Sentence('"The time has come," the Walrus said,')
+        >>> s
+        Sentence('"The time ha... Walrus said,')
+        >>> for word in s:
+        ...     print(word)
+        The
+        time
+        has
+        come
+        the
+        Walrus
+        said
+        >>> list(s)
+        ['The', 'time', 'has', 'come', 'the', 'Walrus', 'said']
+    """
+    
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+        
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+    
+    def __iter__(self):
+        for word in self.words:
+            yield word
+        return
+    
+        
+if __name__=='__main__':
+    doctest.testmod(verbose=True)
