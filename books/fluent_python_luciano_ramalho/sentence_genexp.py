f6333b8073d371ac06d2e92edbf067165a0277cc
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-05-31 10:18:31 +0300

    Create sentence_genexp.py
    implementation of class "Sentence" using a generator expression

---
+++ books/fluent_python_luciano_ramalho/sentence_genexp.py
@@ -0,0 +1,39 @@
+import re
+import reprlib
+import doctest
+
+RE_WORD = re.compile('\w+')
+
+class Sentence:
+    
+    """
+    class ``Sentence``
+    
+        >>> s = Sentence('"The time has come," the Walrus said,')
+        >>> s
+        Sentence('"The time ha... Walrus said,')
+        >>> for word in s:
+        ...     print(word)
+        The
+        time
+        has
+        come
+        the
+        Walrus
+        said
+        >>> list(s)
+        ['The', 'time', 'has', 'come', 'the', 'Walrus', 'said']
+    """
+    
+    def __init__(self, text):
+        self.text = text
+        
+    def __repr__(self):
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+    
+    def __iter__(self):
+        return (match.group() for match in RE_WORD.finditer(self.text))
+    
+        
+if __name__=='__main__':
+    doctest.testmod(verbose=True)
