6e77b148bb2086e83291889b8ca927169e564bd3
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-02 16:30:36 +0300

    Update coroaverager1.py

--- books/fluent_python_luciano_ramalho/coroaverager1.py
+++ books/fluent_python_luciano_ramalho/coroaverager1.py
@@ -1,5 +1,5 @@
 import doctest
-
+from coroutil import coroutine
 """
     A coroutine to compute a running average
     
@@ -10,21 +10,21 @@ import doctest
         10.0
         >>> coro_avg.send(30)
         20.0
-        >> coro_avg.send(5)
+        >>> coro_avg.send(5)
         15.0
-
 """
 
-from couroutil import couroutine
-
-@couroutine
+@coroutine
 def averager():
     total = 0.0
     count = 0
     averager = None
     while True:
         term = yield averager
-        total = += term
-        count = +=1
+        total += term
+        count += 1
         averager = total/count
         
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
+    
