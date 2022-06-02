71f1d55cbb7712ea1e9c9315c733555dc0f8a823
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-02 16:30:55 +0300

    Update coroutil.py

--- books/fluent_python_luciano_ramalho/coroutil.py
+++ books/fluent_python_luciano_ramalho/coroutil.py
@@ -1,9 +1,9 @@
 from functools import wraps
 
-def coutoutine(func):
+def coroutine(func):
     """ Decorator: primes `func` by advancing to first `yield`"""
     @wraps(func)
-    def primer(*args, **kwargs)
+    def primer(*args, **kwargs):
         gen = func(*args, ** kwargs)
         next(gen)
         return gen
