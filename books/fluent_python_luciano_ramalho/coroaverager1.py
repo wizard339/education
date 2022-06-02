495790cc9ac749690c6a3ba28db38bd0bfac3e6f
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-02 16:01:27 +0300

    Create coroaverager1.py
    decorated function "averager"

---
+++ books/fluent_python_luciano_ramalho/coroaverager1.py
@@ -0,0 +1,30 @@
+import doctest
+
+"""
+    A coroutine ro compute a runng average
+    
+        >>> coro_avg = averager()
+        >>> from inspect import getgeneratorstate
+        'GEN_SUSPENDED'
+        >>> coro_avg.send(10)
+        10.0
+        >>> coro_avg.send(30)
+        20.0
+        >> coro_avg.send(5)
+        15.0
+
+"""
+
+from couroutil import couroutine
+
+@couroutine
+def averager():
+    total = 0.0
+    count = 0
+    averager = None
+    while True:
+        term = yield averager
+        total = += term
+        count = +=1
+        averager = total/count
+        
