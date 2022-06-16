cdd3e88bfe5a0a1478191009124a21c6c5152a48
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-16 16:49:42 +0300

    Create osconfeed.py
    script for downloading osconfeed.json

---
+++ books/fluent_python_luciano_ramalho/osconfeed.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+import warnings
+import os
+import json
+
+
+URL = 'https://searchcode.com/codesearch/raw/99786584/'
+JSON = 'data/osconfeed.json'
+
+
+def load():
+    if not os.path.exists(JSON):
+        msg = 'downloading {} to {}'.format(URL, JSON)
+        warnings.warn(msg)
+        with urlopen(URL) as remote, open(JSON, 'wb') as local:
+            local.write(remote.read())
+
+    with open(JSON) as fp:
+        return json.load(fp)
