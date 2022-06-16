730100f0cc38580ab372e7bc3de331d70ef46bea
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-16 13:24:18 +0300

    Update flags2_asyncio.py

--- books/fluent_python_luciano_ramalho/flags2_asyncio.py
+++ books/fluent_python_luciano_ramalho/flags2_asyncio.py
@@ -12,8 +12,8 @@ DEFAULT_CONCUR_REQ = 5
 MAX_CONCUR_REQ = 1000
 
 
-# this exception is needed for wrapping exceptions of web and HTTP,
-# in order to add a field `country_code` to them
+# this exception is needed for wrapping exceptions of web and HTTP, in order to
+# add a field `country_code` to them
 class FetchError(Exception):
     def __init__(self, country_code):
         self.country_code = country_code
@@ -33,10 +33,10 @@ async def get_flag(base_url, cc):
                                           headers=resp.headers)
 
 
-# `semaphore` is object of class `asyncio.Semaphore`, synchronization
-# mechanism that limits the number of concurent requests, so as not to
-# block the whole system: only this coroutine is blocked when the
-# semaphore counter will reaches the maximum allowed value
+# `semaphore` is object of class `asyncio.Semaphore`, synchronization mechanism
+# that limits the number of concurent requests, so as not to block the whole
+# system: only this coroutine is blocked when the semaphore counter will reaches
+# the maximum allowed value
 async def download_one(cc, base_url, semaphore, verbose):
     try:
         # semaphore uses as context manager in await expression
@@ -60,4 +60,51 @@ async def download_one(cc, base_url, semaphore, verbose):
 
 async def downloader_coro(cc_list, base_url, verbose, concur_req):
     counter = collections.Counter()
+    # create a semaphore object, which allows to run no more than `concur_req`      # coroutines at the same time
     semaphore = asyncio.Semaphore(concur_req)
+    to_do = [download_one(cc, base_url, semaphore, verbose)
+            for cc in sorted(cc_list)]
+    
+    # this iterator will return future objects as they are completed
+    to_do_iter = asyncio.as_completed(to_do)
+    if not verbose:
+        to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+
+    for future in to_do_iter:
+        try:
+            res = await future
+        except FetchError as exc:
+            # get the country code from the object, when downloading the flag
+            # of which an exception occurred
+            country_code = exc.country_code
+            # try to get a message about exception from the original exception
+            # object
+            try:
+                error_msg = exc.__cause__.args[0]
+            # if in original exception not message about exception, use a name
+            # of class of original exception
+            except IndexError:
+                error_msg = exc.__cause__.__class__.__name__
+            if verbose and error_msg:
+                msg = '*** Error for {}: {}'
+                print(msg.format(country_code, error_msg))
+            status = HTTPStatus.error
+        else:
+            status = res.status
+
+        counter[status] += 1
+
+    return counter
+
+
+def download_many(cc_list, base_url, verbose, concur_req):
+    loop = asyncio.get_event_loop()
+    coro = downloader_coro(cc_list, base_url, verbose, concur_req)
+    counts = loop.run_until_complete(coro)
+    loop.close()
+
+    return counts
+
+
+if __name__ == '__main__':
+    main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
