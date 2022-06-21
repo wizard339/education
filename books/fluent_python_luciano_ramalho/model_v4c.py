f931012955a1af550e240f3a646314d344324498
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-21 12:44:34 +0300

    Create model_v4c.py
    the descriptor class "Quantity" was moved to a separate module

---
+++ books/fluent_python_luciano_ramalho/model_v4c.py
@@ -0,0 +1,24 @@
+class Quantity:
+    # this attribute is needed to count instances of `Quantity`
+    __counter = 0
+    
+    def __init__(self):
+        cls = self.__class__
+        prefix = cls.__name__
+        index = cls.__counter
+        self.storage_name = '_{}#{}'.format(prefix, index)
+        cls.__counter += 1
+
+    # we have to implement the method `__get__` because the name of the managed attribute
+    # does not match the value `storage_name`
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        if value > 0:
+            setattr(instance, self.storage_name, value)
+        else:
+            raise ValueError('value must be > 0')
