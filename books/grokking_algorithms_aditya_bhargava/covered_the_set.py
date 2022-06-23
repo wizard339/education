0179f36d343403932b9d3cb7d31ea1aa0fab4cb5
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-06-23 22:57:02 +0300

    Create covered_the_set.py

---
+++ books/grokking_algorithms_aditya_bhargava/covered_the_set.py
@@ -0,0 +1,29 @@
+"""
+It is necessary to find a minimum set of stations that would cover the necessary states.
+"""
+
+states_needed = {'mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'}
+
+stations = dict()
+stations['kone'] = {'id', 'nv', 'ut'}
+stations['ktwo'] = {'wa', 'id', 'mt'}
+stations['kthree'] = {'or', 'nv', 'ca'}
+stations['kfour'] = {'nv', 'ut'}
+stations['kfive'] = {'ca', 'az'}
+
+final_stations = set()
+
+while states_needed:
+    best_station = None
+    states_covered = set()
+    
+    for station, states in stations.items():
+        covered = states_needed & states
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+
+    states_needed -= states_covered
+    final_stations.add(best_station)
+
+print(final_stations)
