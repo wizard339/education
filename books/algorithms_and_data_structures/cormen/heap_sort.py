c3cd49f78d2caff5d2f85483e779d84624361da5
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-07-19 16:35:48 +0300

    Update heap_sort.py

--- books/algorithms_and_data_structures/cormen/heap_sort.py
+++ books/algorithms_and_data_structures/cormen/heap_sort.py
@@ -1,7 +1,3 @@
-def parent(i):
-    return i // 2
-
-
 def left(i):
     return 2*i + 1
 
@@ -33,8 +29,21 @@ def build_heap(arr):
         heapify(arr, i)
 
 
+def heapsort(arr):
+    build_heap(arr)
+    global heap_size
+    heap_size = len(arr) - 1
+    for i in range(len(arr) - 1, 1, -1):
+        tmp = arr[0]
+        arr[0] = arr[i]
+        arr[i] = tmp
+        heap_size -= 1
+        heapify(arr, 0)
+
+
 if __name__ == '__main__':
     a = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
     build_heap(a)
     print(a)
-    
+    heapsort(a)
+    print(a)   
