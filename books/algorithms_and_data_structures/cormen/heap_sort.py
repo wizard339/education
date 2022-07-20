85c278b2c4055df2d0177acc6089dc62e5237657
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-07-20 11:31:33 +0300

    Update heap_sort.py

--- books/algorithms_and_data_structures/cormen/heap_sort.py
+++ books/algorithms_and_data_structures/cormen/heap_sort.py
@@ -1,49 +1,62 @@
-def left(i):
-    return 2*i + 1
+class Heapsort:
 
+    def __init__(self):
+        self.arr = []
+        self.heapsize = 0
 
-def right(i):
-    return 2*i + 2
-
-
-def heapify(arr, i):
-    l = left(i)
-    r = right(i)
-    if l <= heap_size and arr[l] > arr[i]:
-        largest = l
-    else:
-        largest = i
-    if r <= heap_size and arr[r] > arr[largest]:
-        largest = r
-    if largest != i:
-        tmp = arr[largest]
-        arr[largest] = arr[i]
-        arr[i] = tmp
-        heapify(arr, largest)
-
-
-def build_heap(arr):
-    global heap_size
-    heap_size = len(arr) - 1
-    for i in range((len(arr) // 2), -1, -1):
-        heapify(arr, i)
-
-
-def heapsort(arr):
-    build_heap(arr)
-    global heap_size
-    heap_size = len(arr) - 1
-    for i in range(len(arr) - 1, 1, -1):
-        tmp = arr[0]
-        arr[0] = arr[i]
-        arr[i] = tmp
-        heap_size -= 1
-        heapify(arr, 0)
-
+    def parent(self, i):
+        return i // 2
+        
+    def left(self, i):
+        return 2*i + 1
+    
+    def right(self, i):
+        return 2*i + 2
+    
+    def heapify(self, i):
+        l = self.left(i)
+        r = self.right(i)
+        if l <= self.heapsize and self.arr[l] > self.arr[i]:
+            largest = l
+        else:
+            largest = i
+        if r <= self.heapsize and self.arr[r] > self.arr[largest]:
+            largest = r
+        if largest != i:
+            # explicit exchange of values
+            tmp = self.arr[largest]
+            self.arr[largest] = self.arr[i]
+            self.arr[i] = tmp
+            self.heapify(largest)
+    
+    
+    def build_heap(self, arr):
+        self.heapsize = len(arr) - 1
+        self.arr = arr
+        for i in range((len(arr) // 2), -1, -1):
+            self.heapify(i)
+    
+    
+    def heapsort(self, arr):
+        self.build_heap(arr)
+        for i in range(len(arr) - 1, 0, -1):
+            tmp = self.arr[0]
+            self.arr[0] = self.arr[i]
+            self.arr[i] = tmp
+            self.heapsize -= 1
+            self.heapify(0)
+            
+    
+    def print_heap(arr):
+        print(arr[0])
+        print(arr[1], arr[2])
+        print(arr[3], arr[4], arr[5], arr[6])
+        print(arr[7], arr[8], arr[9], arr[10], arr[11], arr[12], arr[13])
 
 if __name__ == '__main__':
     a = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
-    build_heap(a)
+    h_sort = Heapsort()
+    h_sort.build_heap(a)
     print(a)
-    heapsort(a)
+    h_sort.heapsort(a)
     print(a)   
