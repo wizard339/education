92bb9f59e3c73c422e041c33ac3c47fc7b7194d6
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-07-22 15:21:24 +0300

    Update quicksort.py
    added randomized version of the quicksort

--- books/algorithms_and_data_structures/cormen/quicksort.py
+++ books/algorithms_and_data_structures/cormen/quicksort.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def quicksort(arr, p, r):
     if p < r:
         q = partition(arr, p, r)
@@ -22,9 +24,26 @@ def partition(arr, p, r):
             return j
 
 
+def randomized_partition(arr, p, r):
+    i = randint(p, r)
+    arr[p], arr[i] = arr[i], arr[p]
+    return partition(arr, p, r)
+
+
+def randomized_quicksort(arr, p, r):
+    if p < r:
+        q = randomized_partition(arr, p, r)
+        randomized_quicksort(arr, p, q)
+        randomized_quicksort(arr, q + 1, r)
+
+
 if __name__ == '__main__':
     a = [13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21]
     print(f"Before: {a}")
     quicksort(a, 0, len(a) - 1)
     print(f"After: {a}")
+    b = a = [13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21]
+    print(f"Before: {b}")
+    randomized_quicksort(b, 0, len(b) - 1)
+    print(f"After: {b}")
     
