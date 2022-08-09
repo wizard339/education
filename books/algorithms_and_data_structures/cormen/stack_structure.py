d3492f28358221fbde6561a9d32efb616efbce39
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-08-09 16:53:00 +0300

    Create stack_structure.py

---
+++ books/algorithms_and_data_structures/cormen/stack_structure.py
@@ -0,0 +1,33 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+        self.top = len(self.stack)
+
+    def stack_empty(self):
+        if self.top == 0: return True
+        else: return False
+
+    def push(self, x):
+        self.top += 1
+        self.stack.append(x)
+
+    def pop(self):
+        if self.stack_empty():
+            raise 'underflow'
+        else:
+            self.top -= 1
+            return self.stack.pop()
+
+
+if __name__=='__main__':
+    b = Stack()
+    print(b.top)
+    print(b.stack_empty())
+    b.push(5)
+    print(b.top)
+    print(b.pop())
+    print(b.top)
+    b.push(5)
+    b.push(5)
+    print(b.top)
