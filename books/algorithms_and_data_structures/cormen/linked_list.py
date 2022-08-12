a3131d0f66d09f29c3a9bba5d6b7b2707868dea3
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-08-12 13:39:33 +0300

    Update linked_list.py

--- books/algorithms_and_data_structures/cormen/linked_list.py
+++ books/algorithms_and_data_structures/cormen/linked_list.py
@@ -1,5 +1,4 @@
 class Node:
-
     def __init__(self, data):
         self.key = data
         self.next = None
@@ -14,24 +13,53 @@ class Node:
 
 
 class DoubleLinkedList:
-
     def __init__(self):
         self.head = None
-        self.tail = None
 
-    def add_item(self, item):
-        if isinstance(item, Node):
-            if self.head is None:
-                self.head = item
-                item.prev = None
-                item.next = None
-                self.tail = item
-            else:
-                self.tail.next = item
-                item.prev = self.tail
-                self.tail = item
-        return
+    def search(self, k):
+        x = self.head
+        while x is not None and x.key != k:
+            x = x.next
+        try:
+            if x.key == k:
+                return x
+        except:
+            raise KeyError('This item is not in the list!')
 
-    def remove_item(self, item_id):
-        pass
-        
+    def insert(self, x):
+        if not isinstance(x, Node):
+            x = Node(x)
+        x.next = self.head
+        if self.head is not None:
+            self.head.prev = x
+        self.head = x
+        x.prev = None
+
+    def delete_by_item(self, x):
+        if x.prev is not None:
+            x.prev.next = x.next
+        else:
+            self.head = x.next
+        if x.next is not None:
+            x.next.prev = x.prev
+
+    def delete_by_key(self, k):
+        x = self.search(k)
+        self.delete_by_item(x)
+
+
+if __name__ == '__main__':
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    linked_list = DoubleLinkedList()
+    linked_list.insert(node1)
+    linked_list.insert(node2)
+    linked_list.insert(node3)
+    print(linked_list.search(2))
+    print(node2)
+    linked_list.insert(10)
+    print(linked_list.search(10))
+    linked_list.delete_by_item(node2)
+    linked_list.delete_by_key(10)
+    print(linked_list.search(10))
