0b8984c8ebd9a3a123dae8a8a37df749e8be4030
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-08-23 15:50:29 +0300

    Update bin_tree_search.py
    added a deletion method

--- books/algorithms_and_data_structures/cormen/bin_tree_search.py
+++ books/algorithms_and_data_structures/cormen/bin_tree_search.py
@@ -58,8 +58,35 @@ class BinTreeSearch:
             print(current.key)
             self.inorder_walk(current.right)
 
-    def delete(self):
-        pass
+    def transplant(self, deleting_node: Optional["Node"], replacing_node: Optional["Node"]) -> None:
+        """the method is needed to replace one subtree with another subtree"""
+        if deleting_node.parent is None:
+            self.root = replacing_node
+        elif deleting_node == deleting_node.parent.left:
+            deleting_node.parent.left = replacing_node
+        else:
+            deleting_node.parent.right = replacing_node
+        if replacing_node:
+            replacing_node.parent = deleting_node.parent
+    
+    def delete(self, deleting_node: Optional["Node"]) -> None:
+        """the method is required to delete a node"""
+        # Case 1: no child or Case 2a: only one right child
+        if deleting_node.left is None:
+            self.transplant(deleting_node, deleting_node.right)
+        # Case 2b: only one left left child
+        elif deleting_node.right is None:
+            self.transplant(deleting_node, deleting_node.left)
+        # Case 3: two children
+        else:
+            successor_node = self.minimum(deleting_node.right)
+            if successor_node.parent != deleting_node:
+                self.transplant(successor_node, successor_node.right)
+                successor_node.right = deleting_node.right
+                successor_node.right.parent = successor_node
+            self.transplant(deleting_node, successor_node)
+            successor_node.left = deleting_node.left
+            successor_node.parent = successor_node
 
     def minimum(self, node: Optional["Node"]) -> Optional["Node"]:
         """the method returns a pointer to the minimum element of the subtree"""
@@ -112,5 +139,12 @@ if __name__=='__main__':
     b.insert(77, 'This is seventy seven')
     b.inorder_walk(b.root)
 
-    print(f'Successor of the node "50": {b.successor(b.search(50))}')
+    b.delete(b.search(30))
+    print('After deleting 30:')
+    b.inorder_walk(b.root)
+    b.delete(b.search(77))
+    print('After deleting 77:')
+    b.inorder_walk(b.root)
+    
+    print(f'Successor of the node "50": {b.maximum(b.search(50))}')
     print(f'Predecessor of the node "50": {b.predecessor(b.search(50))}')
