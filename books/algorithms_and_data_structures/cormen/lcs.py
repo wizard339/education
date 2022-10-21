f0e26e8690a70377e8439ed35530b4004de5299d
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-10-21 11:10:21 +0300

    Update lcs.py

--- books/algorithms_and_data_structures/cormen/lcs.py
+++ books/algorithms_and_data_structures/cormen/lcs.py
@@ -5,11 +5,11 @@ def lcs_length(x: list, y: list) -> int:
     m = len(x)
     n = len(y)
     # array for storing values of the lengths of the subsequences
-    c = [[0 for i in range(m + 1)] for j in range(n + 1)]
+    c = [[0 for i in range(n + 1)] for j in range(m + 1)]
 
-    for i in range(1, m):
+    for i in range(1, m + 1):
 
-        for j in range(1, n):
+        for j in range(1, n + 1):
 
             if x[i - 1] == y[j - 1]:
                 c[i][j] = c[i - 1][j - 1] + 1
