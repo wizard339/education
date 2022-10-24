e9af50ff477f2af0e62ea836507d0ba0ba187a03
wizard339 <54155668+wizard339@users.noreply.github.com>
2022-10-24 16:48:46 +0300

    Update optimal_bst.py

--- books/algorithms_and_data_structures/cormen/optimal_bst.py
+++ books/algorithms_and_data_structures/cormen/optimal_bst.py
@@ -27,12 +27,43 @@ class OptimalBST:
                         self.root[i][j] = r
         return e, self.root
 
-    def contruct_obst(self, root: list) -> str:
-        pass
+    def construct_obst(self, root: list) -> str:
+        k = self.root[1][self.n]
+        print(f"k[{k}] is the root")
+        left, right = [(1, k - 1)], [(k + 1, self.n)]
+        p = [k]
+        
+        while p:
+            
+            if left:
+                i, j = left.pop(0)
+                
+                if j < i:
+                    print(f"d[{j}] is the left child of k[{p[0]}]")
+                else:
+                    k = self.root[i][j]
+                    print(f"k[{k}] is the left child of k[{p[0]}]")
+                    p[:0] = [k]
+                    left.insert(0, (i, k - 1))
+                    right.insert(0, (k + 1, j))
+                    
+            else:
+                i, j = right.pop(0)
+                
+                if j < i:
+                    print(f"d[{j}] is the right child of k[{p.pop(0)}]")
+                else:
+                    k = self.root[i][j]
+                    print(f"k[{k}] is the right child of k[{p.pop(0)}]")
+                    p[:0] = [k]
+                    left.insert(0, (i, k - 1))
+                    right.insert(0, (k + 1, j))        
 
 
 if __name__ == '__main__':
     p = [.15, .10, .05, .10, .20]
     q = [.05, .10, .05, .05, .05, .10]
     obst = OptimalBST(p, q)
-    print(obst.optimal_bst())
+    root = obst.optimal_bst()[1]
+    print(root)
+    print(obst.construct_obst(root))
